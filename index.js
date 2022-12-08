b67f6f46a176372ecb96b82868c93fe34b9c1c3d
solidocean007 <clintonwilliams007@gmail.com>
2022-12-08 10:07:47 -0500

    Complete Exercise 1: Destructuring Arrays

--- index.js
+++ index.js
@@ -3,3 +3,7 @@
 Exercise 1: Using array destructuring, take the first two items from the array and store them in a variable */
 
 const names = ['John', 'Jacob', 'Jingleheimer'];
+
+const [name1, name2, ...rest] = names;
+
+console.log(name1 + name2);
