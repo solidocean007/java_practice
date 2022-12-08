c3905e48c2fa2f452df94eb1cf02c2f23229a916
solidocean007 <clintonwilliams007@gmail.com>
2022-12-08 09:48:18 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+/* Destructuring Arrays
+
+Exercise 1: Using array destructuring, take the first two items from the array and store them in a variable */
+
+const names = ['John', 'Jacob', 'Jingleheimer'];
