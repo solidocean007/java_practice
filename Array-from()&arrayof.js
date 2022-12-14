fd3ac91b9a49c9eaed8fb7b5dca5b927ae011280
solidocean007 <clintonwilliams007@gmail.com>
2022-12-13 19:08:08 -0500

    update

---
+++ Array-from()&arrayof.js
@@ -0,0 +1,6 @@
+const shoesArray = ['Nike SB', 'Emerica', 'Supra', 'Vans'];
+
+const headers = document.querySelectorAll('h5');
+const headerArray = array.from(headers);
+headers.map((node) => console.log(node.textContent));
+// console.log(header);s
---
+++ Using_for_of_with_objects.js
@@ -0,0 +1,11 @@
+const skater = {
+  name: 'JRAD',
+  stance: 'regular',
+  fractures: 5,
+  bonesBroken: 0,
+  ankleInjuries: 57,
+};
+
+for(const [prop, value] of Object.entries(skater)) {
+  console.log(prop, value);
+}
