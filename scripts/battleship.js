76b6e56d5f90e3b65b979182d8f691f893f33be4
solidocean007 <clintonwilliams007@gmail.com>
2023-01-25 20:31:44 -0500

    updated battleship

--- scripts/battleship.js
+++ scripts/battleship.js
@@ -1,49 +1,80 @@
 const shipTwo = 2;
 const shipFive = 5;
 const shipThree = 3;
-const shipFour = 4
+const shipFour = 4;
 
 const boardSize = 10;
 let numberOfFoes = 2;
-const boardLetters = ['A','B','C','D','E','F','G','H','I','J'];
-const boardValues = ['1','2','3','4','5','6','7','8','9','10'];
+const boardLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+const boardValues = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 const shotLocations = [];
 let endGame = false;
-
+let theWay = "";
 
 // This function builds an array of objects that represent the plots of the board.
 function gameBoardData(size) {
-  let gameBoardData = [];
+  let gameBoard = [];
   let plotCount = 1;
-  for(let i = 0; i < size; i++) {
+  for (let i = 0; i < size; i++) {
     let xChar = boardLetters[i];
-    for(let j = 0; j < size; j++) {
+    for (let j = 0; j < size; j++) {
       let yNumber = boardValues[j];
-      gameBoardData.push({
-        index : plotCount,
-        id:xChar + yNumber,
-        plotLetter : xChar,
-        plotNumber : yNumber
-      })
-      plotCount++
+      gameBoard.push({
+        index: plotCount,
+        plotData: {
+          id: xChar + yNumber,
+          occupied: false,
+          hit: false,
+        },
+      });
+      plotCount++;
     }
   }
-  return gameBoardData;
- }
+  return gameBoard;
+}
+let gameBoard = gameBoardData(boardSize); // assign gameBoard to the data of the board.
 
-const plots = gameBoardData(boardSize);
- 
-function shipBuild(ship){
-  for(let i = 0; i < ship; i++){
-    start = Math.floor(Math.random()*100)
-    console.log(start);
-  }
+
+function randomSpotToBuild(boardObject) {
+  do {
+    startBuild = Math.floor(Math.random() * 100);
+  } while(boardObject[startBuild].plotData.occupied === true);
+  console.log(startBuild);
+  return startBuild;
 }
 
-shipBuild(shipFive);
+function whichWay() {
+  theWay = Math.random() > 0.5 ? "vertical" : "horizontal";
+  console.log(theWay);
+  return theWay;
+}
 
-function chooseDirection(){
-  let theWay = (Math.random > .5) ? 'vertical' : 'horizontal'
+function upOrDown() {
+  let downOrUp = Math.random() > 0.5 ? "goDown" : "goUp";
+  console.log(downOrUp);
+  return downOrUp;
+}
+
+function leftOrRight() {
+  let rightOrLeft = Math.random() > 0.5 ? "goLeft" : "goRight";
+  console.log(rightOrLeft);
+  return rightOrLeft;
+}
+
+function shipBuilder2point0(obj) {
+  randomSpotToBuild(obj);
+  whichWay();
+  if (theWay === "vertical") {
+    upOrDown();
+  } else {
+    leftOrRight();
   }
+  obj[startBuild].plotData.occupied = true;
+}
 
-  console.log(chooseDirection())
+for(let i = 0; i < 5; i++){
+  shipBuilder2point0(gameBoard);
+}
+console.log(gameBoard);
+
+// console.log(Math.random() + ' : random number')
